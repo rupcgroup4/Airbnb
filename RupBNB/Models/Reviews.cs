418eb0cab93402a4253ebaa318161d7f130c53e9
Moshemena <moshemcx@gmail.com>
2022-05-20 11:22:46 +0300

    Create class reviews

---
+++ RupBNB/Models/Reviews.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RupBNB.Models
+{
+    public class Reviews
+    {
+        int id;
+        int apartmentId;
+        int reviewerId;
+        string reviewerName;
+        DateTime date;
+        string comments;
+
+        public Reviews(int id, int apartmentId, int reviewerId, string reviewerName, DateTime date, string comments)
+        {
+            this.id = id;
+            this.apartmentId = apartmentId;
+            this.reviewerId = reviewerId;
+            this.reviewerName = reviewerName;
+            this.date = date;
+            this.comments = comments;
+        }
+        public Reviews() { }
+
+        public int Id { get => id; set => id = value; }
+        public int ApartmentId { get => apartmentId; set => apartmentId = value; }
+        public int ReviewerId { get => reviewerId; set => reviewerId = value; }
+        public string ReviewerName { get => reviewerName; set => reviewerName = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public string Comments { get => comments; set => comments = value; }
+    }
+}
