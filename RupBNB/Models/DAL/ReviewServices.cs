4e3d240817a5c514ae45de3da9d6780e48e299d2
Moshemena <moshemcx@gmail.com>
2022-05-21 19:15:59 +0300

    Update ReviewServices

--- RupBNB/Models/DAL/ReviewServices.cs
+++ RupBNB/Models/DAL/ReviewServices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,73 @@ namespace RupBNB.Models.DAL
 {
     public class ReviewServices
     {
-      
+        public int InsertReview(Review review)
+        {
+            SqlConnection con = SqlConnect.Connect();
+
+            // Create Command
+            SqlCommand command = CreateInsertReview(con, review);
+
+            // Execute
+            int numAffected = command.ExecuteNonQuery();
+
+            // Close Connection
+            con.Close();
+
+            return numAffected;
+
+        }
+
+        private SqlCommand CreateInsertReview(SqlConnection con, Review review)
+        {
+
+            SqlCommand command = new SqlCommand();
+
+            command.Parameters.AddWithValue("@id", review.Id);
+            command.Parameters.AddWithValue("@apartmentId", review.ApartmentId);
+            command.Parameters.AddWithValue("@userName", review.UserName);
+            command.Parameters.AddWithValue("@reviewDate", review.ReviewDate);
+            command.Parameters.AddWithValue("@comments", review.Comments);
+
+            command.CommandText = "SP_InsertReservation";
+            command.Connection = con;
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.CommandTimeout = 10; // in seconds
+
+            return command;
+        }
+
+
+        public bool ReviewExists(int id)
+        {
+            SqlConnection con = SqlConnect.Connect();
+
+            // Create Command
+            SqlCommand command = CreateReviewExists(con, id);
+
+            SqlDataReader dr = command.ExecuteReader();
+
+            bool flag = dr.HasRows;
+            con.Close();
+
+            return flag;
+
+
+        }
+
+        private SqlCommand CreateReviewExists(SqlConnection con, int id)
+        {
+            SqlCommand command = new SqlCommand();
+
+            command.Parameters.AddWithValue("@id", id);
+
+            command.CommandText = "SP_GetReviewById";
+            command.Connection = con;
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.CommandTimeout = 10; // in seconds
+
+            return command;
+        }
+
     }
 }
