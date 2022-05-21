ae8d6aaad0e5ef1374ea095e4ab360004fa0d807
Yoav Harel <yoavbkk@gmail.com>
2022-05-21 19:54:20 +0300

    create res and reviews

--- SQL/create_Reservation.sql
+++ SQL/create_Reservation.sql
@@ -2,8 +2,7 @@ create table Reservations (
 	id int identity(1,1) primary key,
 	startDate date not null check(getDate() <= startDate),
 	endDate date not null check(getDate() <= endDate),
-	apartmentId int not null,
-	hostId int not null,
-	userId int not null,
+	apartmentId int foreign key References Apartments(id) not null,
+	userEmail nvarchar(64) foreign key References UsersDB(email) not null,
 	check(startDate <= endDate)
 )
--- SQL/create_Reviews.sql
+++ SQL/create_Reviews.sql
@@ -1,6 +1,6 @@
 create table Reviews ( 
 	id int identity(1,1) primary key,
-	apartmentId int not null,
+	apartmentId int foreign key References Apartments(id) not null,
 	userName nvarchar(30) not null,
 	reviewDate date check(reviewDate <= getdate()),
 	comments nvarchar(MAX)
