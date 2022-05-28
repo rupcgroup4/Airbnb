f71f23bf3b88442f0f1ce65f7871d487727a98d5
Moshemena <moshemcx@gmail.com>
2022-05-28 16:20:59 +0300

    Create admin view html/js

---
+++ RupBNB/Scripts/adminView.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    renderUsersTables();
+});
+var flag_apartmentView = true;
+var flag_hostView = true;
+
+function renderUsersTables() {
+    ajaxCall("GET", "../api/Products/ReadUsers", "", SCBReadUsers, ECBReadUsers);
+}
+function renderHostsTables() {
+    if (flag_hostView) {
+        ajaxCall("GET", "../api/Products/ReadHosts", "", SCBReadHosts, ECBReadHosts);
+        flag_hostView = false;
+        $("#nav-host-tab").prop('onclick', null);
+    }
+}
+function renderApartmentsTables() {
+    if (flag_apartmentView) {
+        ajaxCall("GET", "../api/Products/ReadApartments", "", SCBReadApartments, ECBReadApartments);
+        flag_apartmentView = false;
+        $("#nav-apartment-tab").prop('onclick', null);
+    }
+}
+// Read users success call back
+function SCBReadUsers(users) {
+}
+// Read hosts success call back
+function SCBReadHosts() {
+
+}
+// Read apartments success call back
+function SCBReadApartments() {
+
+}
