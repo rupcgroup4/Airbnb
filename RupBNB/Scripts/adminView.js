36846ab59fc30b01f30c622f515fbe4f313bcf7e
Moshemena <moshemcx@gmail.com>
2022-05-28 16:39:30 +0300

    Update admin view

--- RupBNB/Scripts/adminView.js
+++ RupBNB/Scripts/adminView.js
@@ -23,12 +23,65 @@ function renderApartmentsTables() {
 }
 // Read users success call back
 function SCBReadUsers(users) {
+    try {
+        tbl = $('#UserTable').DataTable({
+            data: users,
+            pageLength: 5,
+            columns: [
+                { data: "Register date" },
+                { data: "Total rentals" },
+                { data: "Total income" },
+                { data: "Total cancels" }
+            ],
+        });
+    }
+    catch (err) {
+        alert(err);
+    }
 }
 // Read hosts success call back
 function SCBReadHosts() {
-
+    try {
+        tbl = $('#HostTable').DataTable({
+            data: users,
+            pageLength: 5,
+            columns: [
+                { data: "Register date" },
+                { data: "Number of apartments" },
+                { data: "Total income" },
+                { data: "Total cancels" }
+            ],
+        });
+    }
+    catch (err) {
+        alert(err);
+    }
 }
 // Read apartments success call back
 function SCBReadApartments() {
-
+    try {
+        tbl = $('#ApartmentTable').DataTable({
+            data: users,
+            pageLength: 5,
+            columns: [
+                { data: "Days rented" },
+                { data: "Total cancels" }
+            ],
+        });
+    }
+    catch (err) {
+        alert(err);
+    }
+}
+// Read users error call back
+function ECBReadUsers(error) {
+    console.log(error);
+}
+// Read hosts error call back
+function ECBReadHosts(error) {
+    console.log(error);
+}
+// Read apartments error call back
+function ECBReadApartments(error) {
+    console.log(error);
 }
