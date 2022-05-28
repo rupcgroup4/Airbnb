e7e2a6d2263dbc45964eca3af3103e6f54d85714
Yoav Harel <yoavbkk@gmail.com>
2022-05-28 12:38:15 +0300

    change price range slider for apartments.html

--- RupBNB/Scripts/apartments.js
+++ RupBNB/Scripts/apartments.js
@@ -57,3 +57,28 @@ function addRoom() {
     }
   
 }
+
+
+
+
+
+// // Code for price range slider
+
+var minSlider = document.getElementById('min');
+var maxSlider = document.getElementById('max');
+
+var outputMin = document.getElementById('min-value');
+var outputMax = document.getElementById('max-value');
+
+outputMin.innerHTML = "$" +  minSlider.value;
+outputMax.innerHTML = "$" + maxSlider.value;
+
+minSlider.oninput = function(){
+ outputMin.innerHTML= "$" +this.value;    
+}
+
+maxSlider.oninput = function(){
+ outputMax.innerHTML= "$" +this.value;    
+}
+
+
