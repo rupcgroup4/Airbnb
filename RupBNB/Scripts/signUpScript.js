878c27f3faa6a664e18b3f7b8fe07b33572946df
stavHerzog <stavherzog24@gmail.com>
2022-05-28 16:40:26 +0300

    up Form and add SignUpScript

---
+++ RupBNB/Scripts/signUpScript.js
@@ -0,0 +1,44 @@
+﻿
+$(document).ready(function() {
+
+	$("#signUpForm").submit(submitSignUpForm);
+	
+	
+	//$("#loginBTN").click(function () {
+	//	window.location.replace('login.html');
+	//})
+
+});
+
+
+//submit sign up form function
+//fetch the data of the user input and send api call to store the user data in DB
+function submitSignUpForm() {
+
+	let email = $("#email").val();
+	let password = $("#password").val();
+	let firstName = $("#firstName").val();
+	let lastName = $("#lastName").val();
+	let birthDate = $("#birthDate").val();
+
+	let newUser = {
+		Email: email,
+		Password: password,
+		FirstName: firstName,
+		LastName: lastName,
+		BirthDate: birthDate,
+	}
+
+	ajaxCall("POST", "../api/Users", JSON.stringify(newUser), submitSignUpFormSuccess, submitSignUpFormError);
+
+	return false;
+}
+
+function submitSignUpFormSuccess(result)
+{
+	consol.log(result);
+}
+
+function submitSignUpFormError(err) {
+	alert(err);
+}
