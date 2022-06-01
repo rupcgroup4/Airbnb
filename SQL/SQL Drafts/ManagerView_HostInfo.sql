70aaeff61f65b0838578fee971f832e6ea9dbd3d
Moshemena <moshemcx@gmail.com>
2022-06-01 16:57:14 +0300

    update admin view host sql

--- SQL/SQL Drafts/ManagerView_HostInfo.sql
+++ SQL/SQL Drafts/ManagerView_HostInfo.sql
@@ -1,19 +1,22 @@
-
-DECLARE @userEmail nvarchar(64)
-SET @userEmail='Abhishek72@gmail.com'
+WITH MyTable AS 
+(
 select T1.email, T1.hostSince,T1.NumOfApartments, T1.TotalCanceled, T2.TotalPrice
 from 
 	(select H.email, H.hostSince, count(distinct A.id) as NumOfApartments,(count(*)-sum(CAST(R.isCanceled AS INT))) as TotalRents, sum(CAST(R.isCanceled AS INT)) as TotalCanceled
 	from Hosts as H inner join Reservations as R on H.email=R.userEmail
 		inner join Apartments as A on A.id=R.apartmentId
-	where H.email=@userEmail 
 	GROUP BY H.email, H.hostSince) T1 
 	inner join 
 	(select R.userEmail, sum((DATEDIFF(day, R.startDate, R.endDate)*A.price)) TotalPrice
 	from Reservations as R inner join Apartments as A on R.apartmentId=A.id
-	where R.userEmail=@userEmail and R.isCanceled=0
+	where  R.isCanceled=0
 	group by R.userEmail) T2 on T1.email=T2.userEmail
-
+)
+select H.email,H.hostSince, 
+ISNULL(MT.NumOfApartments,0) as TotalRents, 
+ISNULL(MT.TotalCanceled,0) AS TotalCanceled,
+ISNULL(MT.TotalPrice,0) as TotalPrice
+from MyTable AS MT right join Hosts AS H on MT.email = H.email 
 
 
 
