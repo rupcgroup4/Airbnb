e326e68281a56a58bad3e30e2471552c73b98955
Yoav Harel <yoavbkk@gmail.com>
2022-07-02 15:53:13 +0300

    birthdate in signup must be 18+ (not working)

--- RupBNB/Scripts/signUpScript.js
+++ RupBNB/Scripts/signUpScript.js
@@ -36,6 +36,22 @@ function submitSignUpForm() {
 	return false;
 }
 
+
+function check18Age() {
+    var today = new Date();
+    var birthDate = new Date($("#birthDate").val());
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+	if(age < 18) {
+		return false;
+	}
+	return true;
+}
+
 function submitSignUpFormSuccess(user)
 {
 	localStorage.setItem("CGroup4_user", JSON.stringify(user));
