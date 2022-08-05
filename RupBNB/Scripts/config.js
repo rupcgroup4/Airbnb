e16864492e7eb9d97b9467f6ad065e183faad1b1
Yoav Harel <88085119+HarelYoav@users.noreply.github.com>
2022-08-05 10:11:10 +0300

    Update config.js

--- RupBNB/Scripts/config.js
+++ RupBNB/Scripts/config.js
@@ -2,14 +2,14 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAGleI4qjrVJYkIYoJxnSPNH7vSULRbPmQ",
-    authDomain: "mydashboard-326509.firebaseapp.com",
-    databaseURL: "https://mydashboard-326509-default-rtdb.firebaseio.com",
-    projectId: "mydashboard-326509",
-    storageBucket: "mydashboard-326509.appspot.com",
-    messagingSenderId: "415049196836",
-    appId: "1:415049196836:web:8cafd6a6901949e385bfb1",
-    measurementId: "G-FQT8MVHBRN"
+    apiKey: "YOUR API KEY",
+    authDomain: "AUTH DOMAN",
+    databaseURL: "DATA BASE URL",
+    projectId: "PROJECT ID",
+    storageBucket: "YOUR DETAILS",
+    messagingSenderId: "YOUR DETAILS",
+    appId: "YOUR DETAILS",
+    measurementId: "YOUR DETAILS"
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
